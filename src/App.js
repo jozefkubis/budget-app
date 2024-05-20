4274c225ab192ec2a3c3c5252b12a142d6af7d0f
JozefK <164421631+jozefkubis@users.noreply.github.com>
2024-05-20 11:58:25 +0200

    Vytvorenie prvotnej struktury

--- src/App.js
+++ src/App.js
@@ -2,8 +2,69 @@ import { useState } from "react"
 
 export default function App() {
   return (
-    <div>
-      <h1>Budget Tracker</h1>
+    <div className="app">
+      <Header />
+      <Balnce />
+      <Today />
+      <Monthly />
+      <Add />
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Expense Tracker</h1>
+    </div>
+  )
+}
+
+function Balnce() {
+  return (
+    <div className="balance">
+      <Income />
+      <Expense />
+    </div>
+  )
+}
+
+function Income() {
+  return (
+    <div className="income">
+      <h2>Income</h2>
+    </div>
+  )
+}
+
+function Expense() {
+  return (
+    <div className="expense">
+      <h2>Expense</h2>
+    </div>
+  )
+}
+
+function Today() {
+  return (
+    <div className="today">
+      <h2>Today</h2>
+    </div>
+  )
+}
+
+function Monthly() {
+  return (
+    <div className="monthly">
+      <h2>Monthly</h2>
+    </div>
+  )
+}
+
+function Add() {
+  return (
+    <div className="add">
+      <h2>Add new transaction</h2>
     </div>
   )
 }
